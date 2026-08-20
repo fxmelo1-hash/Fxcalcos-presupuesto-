1bfe398a8bf8a08e7f0f204b2b87ce55dbe66ffb
fxmelo1-hash <fxmelo1@gmail.com>
2026-08-20 20:39:46 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fxcalcos-cache-v13';
+const CACHE_NAME = 'fxcalcos-cache-v15';
 
 // Archivos propios de la app: se guardan en caché apenas se instala
 const ARCHIVOS_PROPIOS = [
